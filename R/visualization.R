4654180d9b142810b04414082e1f302724a5d77e
Andrew Ghazi <andghazi@gmail.com>
2021-08-03 19:37:24 -0400

    initial commit for hit calling plot

---
+++ R/visualization.R
@@ -0,0 +1,12 @@
+#' @title Make a hit calling plot
+#' @description Show both the
+hit_calling_plot = function(protein, strain,
+                            bead_binding,
+                            concordances){
+
+  both_plot = a + b
+  plot_list = list(a = interval_plot,
+                   b = entropy plot,
+                   ab = both_plot)
+  return(plot_list)
+}
