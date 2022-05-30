9657f422cc320d03fa1e5c71861576df04ee1015
Andrew Ghazi <andghazi@gmail.com>
2022-05-30 19:05:31 -0400

    turn off this message for now

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,5 @@
   ver = utils::packageVersion("basehitmodel")
   packageStartupMessage("This is basehitmodel version ", ver)
   packageStartupMessage("- Parallelize: Before calling basehitmodel::model_proteins_separately(), run future::plan() in a way that's appropriate for your system.")
-  packageStartupMessage("- Show progress: Before calling basehitmodel::model_proteins_separately(), run library(progressr); handlers(global=TRUE)")
+  # packageStartupMessage("- Show progress: Before calling basehitmodel::model_proteins_separately(), run library(progressr); handlers(global=TRUE)")
 }
