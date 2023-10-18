2fe4c043ceb4176f298cdc036f0230483c2aec6a
Andrew Ghazi <andghazi@gmail.com>
2023-10-18 14:44:59 -0400

    demo data prep

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,3 +4,6 @@
 # $ xz --version
 # xz (XZ Utils) 5.2.5
 # liblzma 5.2.5
+
+# The data in demo/ are the first 1000 columns of the first three files i.e. uncompress the files with xz -kd and then:
+# cut -f -1000 -d, Mapped_bcs_run1.csv > demo1.csv
